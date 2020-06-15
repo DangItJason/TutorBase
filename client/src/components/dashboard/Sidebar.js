16769ebdd6aeb1d297da3042b315d4f2c87d5309
Jeremy Weiss <jweiss045@gmail.com>
2020-06-14 22:30:03 -0400

    Fixed bootstrap div class spelling

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Sidebar extends Component {
     render() {
         return (
-            <div class="big-light border-right" id="sidebar-wrapper">
+            <div class="bg-light border-right" id="sidebar-wrapper">
                 <div class="sidebar-heading">TutorBase</div>
                 <div class="list-group list-group-flush">
                     <a href="#" class="list-group-item list-group-item-action bg-light">Settings</a>
