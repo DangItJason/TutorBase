753da2be179ba642481a26e14795da54598af49c
Jeremy Weiss <jweiss045@gmail.com>
2020-06-15 11:37:38 -0400

    Custom dashboard panel styling

--- client/src/components/dashboard/Panel.js
+++ client/src/components/dashboard/Panel.js
@@ -5,7 +5,7 @@ class Panel extends Component {
         return (
             <div id="panel-wrapper">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                    <button class="btn btn-primary" id="menu-toggle" onClick={this.props.action}><span class="navbar-toggler-icon"></span></button>
+                    <button class="btn bg-red btn-red" id="menu-toggle" onClick={this.props.action}><span class="navbar-toggler-icon"></span></button>
                 </nav>
                 <div class="container-fluid">
                     <h1 class="mt-4">Client Dashboard</h1>
