846f273ee135d612053ca3a2138d6d81a672d4ec
Jeremy Weiss <jweiss045@gmail.com>
2020-06-14 22:28:41 -0400

    Implemented sidebar component

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 class Sidebar extends Component {
     render() {
         return (
-            <div>
-                
+            <div class="big-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">TutorBase</div>
+                <div class="list-group list-group-flush">
+                    <a href="#" class="list-group-item list-group-item-action bg-light">Settings</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light">History</a>
+                </div>
             </div>
         );
     }
