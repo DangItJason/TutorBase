e724993067f2c50af420d4e41fdb9f45588b5819
Jeremy Weiss <jweiss045@gmail.com>
2020-06-14 22:34:35 -0400

    Implemented static panel component

--- client/src/components/dashboard/Panel.js
+++ client/src/components/dashboard/Panel.js
@@ -3,8 +3,14 @@ import React, { Component } from "react";
 class Panel extends Component {
     render() {
         return (
-            <div>
-
+            <div id="panel-wrapper">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle"><span class="navbar-toggler-icon"></span></button>
+                </nav>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Client Dashboard</h1>
+                    <p>This is where the client will be able to schedule tutoring sessions.</p>
+                </div>
             </div>
         );
     }
