50acc2dfccea01eab84a3fe2f43883211e32a284
Jeremy Weiss <jweiss045@gmail.com>
2020-06-15 11:29:52 -0400

    Custom sidebar styling

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 class Sidebar extends Component {
     render() {
         return (
-            <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="bg-red border-right" id="sidebar-wrapper">
                 <div class="sidebar-heading">TutorBase</div>
                 <div class="list-group list-group-flush">
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Settings</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">History</a>
+                    <a href="#" class="list-group-item bg-red list-group-item-action">Settings</a>
+                    <a href="#" class="list-group-item bg-red list-group-item-action">History</a>
                 </div>
             </div>
         );
