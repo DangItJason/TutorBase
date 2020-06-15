44d4ff33ba4e77010a8c592c15824a171f66f1b6
Jeremy Weiss <jweiss045@gmail.com>
2020-06-15 02:00:27 -0400

    Added sidebar functionality

--- client/src/components/dashboard/ClientDashboard.js
+++ client/src/components/dashboard/ClientDashboard.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import classNames from "classnames";
 import Sidebar from "./Sidebar"
 import Panel from "./Panel"
 
 class ClientDashboard extends Component {
+    constructor(props) {
+        super(props)
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.state = { sidebarToggled: false };
+    }
+
+    toggleMenu = () => {
+        this.setState({ sidebarToggled: !this.state.sidebarToggled })
+    }
+
     render() {
         return (
-            <div className="ClientDashboard">
+            <div className={classNames("d-flex", (this.state.sidebarToggled) ? "toggled":"")} id="wrapper">
                 <Sidebar />
-                <Panel />
+                <Panel action={this.toggleMenu} />
             </div>
         );
     }
