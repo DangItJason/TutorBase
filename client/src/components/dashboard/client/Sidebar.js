156ff17e36badb64923d0c1c237f6e8f246ec8c9
Jeremy Weiss <jweiss045@gmail.com>
2020-06-15 21:24:56 -0400

    Added logout/switch dashboard buttons and fontawesome icons

--- client/src/components/dashboard/client/Sidebar.js
+++ client/src/components/dashboard/client/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import { ListGroup, ListGroupItem } from "reactstrap"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog, faHistory, faRandom, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 class Sidebar extends Component {
     render() {
@@ -8,8 +10,12 @@ class Sidebar extends Component {
             <div className={classNames("bg-none", "border-right")} id="sidebar-wrapper">
                 <div class="sidebar-heading">TutorBase</div>
                 <ListGroup>
-                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}>Settings</ListGroupItem>
-                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}>History</ListGroupItem>
+                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}><FontAwesomeIcon icon={faCog} />Settings</ListGroupItem>
+                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}><FontAwesomeIcon icon={faHistory} />History</ListGroupItem>
+                </ListGroup>
+                <ListGroup className="list-group-bottom">
+                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}><FontAwesomeIcon icon={faRandom} />Switch Dashboard</ListGroupItem>
+                    <ListGroupItem tag="a" href="#" className={classNames("list-group-item", "bg-none")}><FontAwesomeIcon icon={faSignOutAlt} />Logout</ListGroupItem>
                 </ListGroup>
             </div>
         );
