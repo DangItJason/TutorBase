8bae9b4cdd89e8116a893644b75a9e8b40538256
Jeremy Weiss <jweiss045@gmail.com>
2020-06-17 19:55:06 -0400

    Base files for Tutor Card components

---
+++ client/src/components/tutorCard/TutorCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+
+class TutorCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TutorCard;
