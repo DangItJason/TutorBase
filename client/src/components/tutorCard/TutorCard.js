57fe89f4b34bcc3901f690df1c6ac3ba3f7f006c
Jeremy Weiss <jweiss045@gmail.com>
2020-06-20 16:34:20 -0400

    Bootstrap card implementation

--- client/src/components/tutorCard/TutorCard.js
+++ client/src/components/tutorCard/TutorCard.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
-import classNames from "classnames";
+import './tutorCard.css'
 
 class TutorCard extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="card col-md-3 mb-4 ml-3 mr-3 text-center">
+                <div class="row">
+                    <div class="col-md-5">
+                        <img class="card-img mt-3" src="#" alt="Tutor image"></img>
+                    </div>
+                    <div class="col-md-7 card-body">
+                        <h4 class="card-title">Tutor Name</h4>
+                    </div>
+                </div>
+                <hr></hr>
+                <p class="card-text">Next available: Time</p>
+                <a href="#" class="btn btn-danger mb-3">Book Now</a>
             </div>
         );
     }
