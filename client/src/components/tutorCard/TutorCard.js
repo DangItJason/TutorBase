d329d74788e60547dbbb6bbabadc1388849e1925
Jeremy Weiss <jweiss045@gmail.com>
2020-06-20 16:36:05 -0400

    Pull tutor information passed from parent

--- client/src/components/tutorCard/TutorCard.js
+++ client/src/components/tutorCard/TutorCard.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import './tutorCard.css'
 
 class TutorCard extends Component {
     render() {
         return (
-            <div className="card col-md-3 mb-4 ml-3 mr-3 text-center">
+            <div class="card col-md-3 mb-4 ml-3 mr-3 text-center">
                 <div class="row">
                     <div class="col-md-5">
-                        <img class="card-img mt-3" src="#" alt="Tutor image"></img>
+                        <img class="card-img mt-3" src={this.props.data.img} alt="Tutor image"></img>
                     </div>
                     <div class="col-md-7 card-body">
-                        <h4 class="card-title">Tutor Name</h4>
+                        <h4 class="card-title">{this.props.data.name}</h4>
                     </div>
                 </div>
                 <hr></hr>
-                <p class="card-text">Next available: Time</p>
+                <p class="card-text">Next available: {this.props.data.available}</p>
                 <a href="#" class="btn btn-danger mb-3">Book Now</a>
             </div>
         );
