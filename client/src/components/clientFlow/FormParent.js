138598e405618e49a98f8f40e6c5f96b0d1b7be2
Jeremy Weiss <jweiss045@gmail.com>
2020-06-24 01:34:29 -0400

    Added state properties and state change handler

--- client/src/components/clientFlow/FormParent.js
+++ client/src/components/clientFlow/FormParent.js
@@ -4,7 +4,19 @@ class FormParent extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            currentStep: 1,
+            subject: "",
+            class: "",
+            tutorID: "",
+            day: "",
+            time: ""
+        }
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target
+        this.setState({[name]: value})
     }
 
     render() {
