50b1a95a50ff3bf72a508ff55538cfc58e1f686a
Jeremy Weiss <jweiss045@gmail.com>
2020-06-24 01:20:43 -0400

    Initial form parent file

---
+++ client/src/components/clientFlow/FormParent.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+
+class FormParent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+
+export default FormParent;
