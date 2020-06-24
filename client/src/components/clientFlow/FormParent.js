f8d6b8744998cac9a7a2c6eaf0236339314c24ba
Jeremy Weiss <jweiss045@gmail.com>
2020-06-24 01:44:20 -0400

    Import form step components from clientFlow

--- client/src/components/clientFlow/FormParent.js
+++ client/src/components/clientFlow/FormParent.js
@@ -1,4 +1,11 @@
 import React, { Component, Fragment } from "react";
+import Step1 from "./1_subject";
+import Step2 from "./2_class";
+import Step3 from "./3_selectTutor";
+import Step4 from "./4_selectDate";
+import Step5 from "./5_selectTime";
+import Step6 from "./6_reserve";
+
 
 class FormParent extends Component {
     constructor(props) {
@@ -9,20 +16,39 @@ class FormParent extends Component {
             subject: "",
             class: "",
             tutorID: "",
-            day: "",
+            date: "",
             time: ""
         }
     }
 
     handleChange = event => {
-        const {name, value} = event.target
-        this.setState({[name]: value})
+        const { name, value } = event.target
+        this.setState({ [name]: value })
     }
 
     render() {
         return (
             <Fragment>
-
+                <form>
+                    <Step1 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                    <Step2 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                    <Step3 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                    <Step4 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                    <Step5 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                    <Step6 currentStep={this.state.currentStep} 
+                        handleChange={this.handleChange}
+                        subject = {this.state.subject} />
+                </form>
             </Fragment>
         );
     }
