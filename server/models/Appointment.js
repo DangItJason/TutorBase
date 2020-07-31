40bee6a0cb63215309f3307faa03159f6690b763
Jeremy Weiss <jweiss045@gmail.com>
2020-07-31 16:13:06 -0400

    Updated User and Appointment models

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -8,7 +8,8 @@ const AppointmentSchema = new Schema({
     start_time: {type: String, required: true},
     end_time: {type: String, required: true},
     location: {type: String, required: true},
-    tutor_id: {type: String, required: true},
+    tutor_id: {type: Schema.ObjectId, required: true}, // Object ID of tutor
+    client_id: {type: Schema.ObjectId, required: true}, // Object ID of client
     price: {type: Number, required: true},
     notes: String,
     confirmed: {type: Boolean, default: false}
