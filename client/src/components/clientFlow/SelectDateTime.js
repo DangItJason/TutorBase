2182a099674e2ce47b25398e2d9bb1031f03f1a5
teetle <unitedbagels@gmail.com>
2020-10-04 18:27:59 -0400

    React schedule selector frontend working

--- client/src/components/clientFlow/SelectDateTime.js
+++ client/src/components/clientFlow/SelectDateTime.js
@@ -1,34 +1,30 @@
 import React, { Component } from "react";
 import AvailableTimes from 'react-available-times';
+import ScheduleSelector from 'react-schedule-selector'
 
 class Step4 extends Component {
 
-    render() {
-        // Only render this step if currentStep matches
-        if (this.props.currentStep !== 4) 
-            return null;
-        return null;
+    state = { schedule : [] }
 
-        /*(
-            <div class="form-group text-center">
-                <h3 class="hr mt-1">Select a Time</h3>
-                <AvailableTimes
-                    timeConvention="12h"
-                    weekStartsOn="sunday"
-                    onChange={(selections) => {
-                        selections.forEach(({ start, end }) => {
-                            console.log('Start:', start, 'End:', end);
-                        })
-                        if (selections.length > 0) {
-                            this.props.handleChangeTime();
-                        }
-                    }}
-                    recurring={false}
-                    availableDays={['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']}
-                    availableHourRange={{ start: 10, end: 20 }}
-                />
-            </div>
-        );*/
+    handleChange = newSchedule => {
+        this.setState({ schedule: newSchedule })
+    }
+
+    render() {
+
+        if (this.props.currentStep !== 4)
+            return null;
+
+        return (
+            <ScheduleSelector
+        selection={this.state.schedule}
+        numDays={5}
+        minTime={8}
+        maxTime={22}
+        hourlyChunks={1}
+        onChange={this.handleChange}
+        />
+    )
     }
 }
 
