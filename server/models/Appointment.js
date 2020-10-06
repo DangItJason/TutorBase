8ff53eee344e3190d8879b3b9d9da8f77e7c2117
Jeremy Weiss <jweiss045@gmail.com>
2020-10-05 23:02:13 -0400

    Store appointment start/end times as Date objects

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -5,8 +5,8 @@ const AppointmentSchema = new Schema({
     appt_id: {type: String, required: true},
     course_id: {type: String, required: true},
     date: {type: Date, required: true},
-    start_time: {type: String, required: true},
-    end_time: {type: String, required: true},
+    start_time: {type: Date, required: true},
+    end_time: {type: Date, required: true},
     location: {type: String, required: true},
     tutor_id: {type: Schema.ObjectId, required: true}, // Object ID of tutor
     client_id: {type: Schema.ObjectId, required: true}, // Object ID of client
