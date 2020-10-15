9a3323fac7b69bf4a2d7b0cabad54452d7cd5af5
gajjas <gajjas@rpi.edu>
2020-10-14 23:48:34 -0400

    Update Price Tutor Operation Route

--- server/routes/tutor-operations.js
+++ server/routes/tutor-operations.js
@@ -1,12 +1,21 @@
 const express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const Subject = require('../../models/Subject');
-const Course = require('../../models/Course');
-const Appointment = require('../../models/Appointment');
-const User = require('../../models/User');
+const Subject = require('../models/Subject');
+const Course = require('../models/Course');
+const Appointment = require('../models/Appointment');
+const User = require('../models/User');
 
+mongoose.set('useFindAndModify', false);
 
+router.put('/price', async (req, res) => {
+    try {
+        await User.findOneAndUpdate(req.body.user, { tutor: { price: req.body.price } })
+        res.send(true)
+    } catch (err) {
+        res.status(400).json({ msg: err.message })
+    }
+});
 
 
 
