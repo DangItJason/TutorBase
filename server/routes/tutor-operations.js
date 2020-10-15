c0579debe33706fe1230e50317e6edae5752103f
Chris <cjreed121@gmail.com>
2020-10-14 23:47:41 -0400

    Added get price route.

--- server/routes/tutor-operations.js
+++ server/routes/tutor-operations.js
@@ -1,12 +1,14 @@
 const express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const Subject = require('../../models/Subject');
-const Course = require('../../models/Course');
-const Appointment = require('../../models/Appointment');
-const User = require('../../models/User');
+const Subject = require('../models/Subject');
+const Course = require('../models/Course');
+const Appointment = require('../models/Appointment');
+const User = require('../models/User');
 
-
+router.get('/price', (req, res) => {
+    User.find({email: req.body.email}).then(users => res.json(users[0].tutor.price)).catch(err => res.status(400).json({ msg: err.message }));
+});
 
 
 
