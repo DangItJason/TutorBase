5bba8d58beabb14bd01c346e0796771da615c573
Chris <cjreed121@gmail.com>
2020-10-21 22:17:46 -0400

    Fixed price set. Added interval get and set.

--- server/routes/tutor-operations.js
+++ server/routes/tutor-operations.js
@@ -9,12 +9,14 @@ const User = require('../models/User');
 mongoose.set('useFindAndModify', false);
 
 router.get('/price', (req, res) => {
-    User.find({ email: req.body.email }).then(users => res.json(users[0].tutor.price)).catch(err => res.status(400).json({ msg: err.message }));
+    User.find({ email: req.body.email })
+        .then(users => res.json(users[0].tutor.price))
+        .catch(err => res.status(400).json({ msg: err.message }));
 });
 
 router.put('/price', async (req, res) => {
     try {
-        await User.findOneAndUpdate(req.body.user, { tutor: { price: req.body.price } })
+        await User.findOneAndUpdate({ email: req.body.email}, {$set: {"tutor.price": req.body.price}})
         res.send(true)
     } catch (err) {
         res.status(400).json({ msg: err.message })
@@ -26,6 +28,22 @@ router.get('/courses', (req, res) => {
         .then(users => res.json(users[0].tutor.courses))
         .catch(err => res.status(400).json({ msg: err.message }));
 });
+
+router.get('/interval', (req, res) => {
+    User.find({email: req.body.email})
+        .then(users => res.json(users[0].tutor.interval))
+        .catch(err => res.status(400).json({ msg: err.message }))
+});
+
+router.put('/interval', async (req, res) => {
+    try {
+        await User.findOneAndUpdate({ email: req.body.email }, {$set: {"tutor.interval": req.body.interval}})
+        res.send(true)
+    } catch (err) {
+        res.status(400).json({ msg: err.message })
+    }
+});
+
 
 
 module.exports = router;
