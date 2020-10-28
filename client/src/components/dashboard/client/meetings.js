8c672ad07ff0e9b24c5fd80de9b55dc6889cb4a2
DangItJason <nguyenjason06@gmail.com>
2020-10-28 16:19:21 -0400

    color & header change to dropdown

--- client/src/components/dashboard/client/meetings.js
+++ client/src/components/dashboard/client/meetings.js
@@ -1,57 +1,66 @@
 import React, { Component } from "react";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faLeaf } from "@fortawesome/free-solid-svg-icons";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import "./meetings.css";
 
 class Meetings extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.changeValue = this.changeValue.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      dropdownValue: "Completed",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.changeValue = this.changeValue.bind(this);
-        this.state = {
-            dropdownOpen: false,
-            dropdownValue: "",
-        }
-    }
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  };
 
-    toggle = () => {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen,
-        })
-    }
+  changeValue(e) {
+    this.setState({
+      dropDownValue: e.currentTarget.textContent,
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
 
-    changeValue(e) {
-        this.setState({ 
-            dropDownValue: e.currentTarget.textContent,
-            dropdownOpen: !this.state.dropdownOpen
-         })
-    }
-
-    render() {
-        return (
-            <Container fluid>
-                <Row className="title">
-                    <div class="profile-text">Settings</div>
-                </Row>
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                    <DropdownToggle caret>
-                        {this.state.dropDownValue}
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem header><div onClick={this.changeValue} >Completed</div></DropdownItem>
-                        <DropdownItem header><div onClick={this.changeValue} >Pending</div></DropdownItem>
-                        <DropdownItem header><div onClick={this.changeValue} >Upcoming</div></DropdownItem>
-                        <DropdownItem header><div onClick={this.changeValue} >Declined</div></DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-                <hr></hr>
-
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container fluid>
+        <Row className="title">
+          <div class="profile-text">Settings</div>
+        </Row>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle caret color="secondary" outline>
+            {this.state.dropDownValue}
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>
+              <div onClick={this.changeValue}>Completed</div>
+            </DropdownItem>
+            <DropdownItem>
+              <div onClick={this.changeValue}>Pending</div>
+            </DropdownItem>
+            <DropdownItem>
+              <div onClick={this.changeValue}>Upcoming</div>
+            </DropdownItem>
+            <DropdownItem>
+              <div onClick={this.changeValue}>Declined</div>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <hr></hr>
+      </Container>
+    );
+  }
 }
 
 export default Meetings;
