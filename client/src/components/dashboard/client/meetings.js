210361fed323f763bcb9f6519432e938ba031fcf
DangItJason <nguyenjason06@gmail.com>
2020-11-11 19:09:43 -0500

    Notes section for client cards + css

--- client/src/components/dashboard/client/meetings.js
+++ client/src/components/dashboard/client/meetings.js
@@ -30,24 +30,28 @@ class Meetings extends Component {
           color: "Completed",
           location: "Barton",
           time: "2PM-4PM",
+          notes: "Hi, I need help with DS really badly."
         },
         {
           name: "Jeremy Weiss",
           color: "Pending",
           location: "Union",
           time: "12PM-4PM",
+          notes: "TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TESTTEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST"
         },
         {
           name: "David Yao",
           color: "Upcoming",
           location: "DCC 308",
           time: "10AM-8PM",
+          notes: "N/A"
         },
         {
           name: "Jacob Zamani",
           color: "Denied",
           location: "Off-Campus",
           time: "8PM-10PM",
+          notes: ""
         },
       ],
     });
