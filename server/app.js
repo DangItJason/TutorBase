354dd42b4ba97e4bcb3dd62ce96d4c0c4d690c32
gajjas <gajjas@rpi.edu>
2021-02-24 22:18:16 -0500

    Changed some auth settings and added some logs

--- server/app.js
+++ server/app.js
@@ -61,11 +61,13 @@ app.use(passport.session());
 
 
 passport.serializeUser(function (user, done) {
-  done(null, user);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (user, done) {
-  done(err, user);
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 
