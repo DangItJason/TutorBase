f9ff1bb209156d3c830f5ba435f9dc0b7ae80511
Jacob Zamani <jacob.zamani@yahoo.com>
2021-02-26 10:36:26 -0700

    feat: Testing js-cookies to read CAS

---
+++ client/src/components/dashboard/client/ClientDashboard.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import Sidebar from "./Sidebar";
+import Panel from "./Panel";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../dashboard.css';
+import {connect} from "react-redux";
+
+import cookies from "js-cookies";
+const secure = window.location.protocol === 'https'
+
+class ClientDashboard extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        console.log(cookies)
+
+        return (
+            <div className={classNames("d-flex", (this.props.sidebarToggled) ? "toggled":"")} id="dashboard-wrapper">
+                <Sidebar />
+                <Panel />
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    const { clientFlow } = state;
+    return { sidebarToggled:  clientFlow.sidebarToggled}
+}
+
+export default connect(mapStateToProps)(ClientDashboard);
