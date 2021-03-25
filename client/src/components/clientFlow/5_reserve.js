a7d0c6a767b51de7fd2b3226d0852e8515f156c3
Jacob Zamani <jacob.zamani@yahoo.com>
2021-03-25 15:17:04 -0400

    feat: Fixed appt post from frontend

--- client/src/components/clientFlow/5_reserve.js
+++ client/src/components/clientFlow/5_reserve.js
@@ -38,23 +38,10 @@ class Step5 extends Component {
       let endMin = ("0" + start.getMinutes()).slice(-2);
       let endHour = ("0" + start.getHours()).slice(-2);
 
-      // let body = {
-      //   course_id: this.props.flowData.courseId,
-      //   date: this.props.flowData.date,
-      //   start: startHour + ":" + startMin,
-      //   end: endHour + ":" + endMin,
-      //   loc: this.props.flowData.apptLoc,
-      //   tutor_id: this.props.flowData.tutorId,
-      //   client_id: this.props.flowData.clientId,
-      //   price: 50,
-      //   notes: this.props.flowData.notes,
-      // };
-
       let body = {
         course_id: this.props.flowData.courseId,
-        date: this.props.flowData.date,
-        start: startHour + ":" + startMin,
-        end: endHour,
+        start: start,
+        end: endHour + endMin,
         loc: this.props.flowData.apptLoc,
         tutor_id: this.props.flowData.tutorId,
         client_id: this.props.flowData.clientId,
