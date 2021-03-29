0c2dd0e4e332e4eb7e394897f95b73257418e589
gajjas <gajjas@rpi.edu>
2021-03-29 15:54:51 -0400

    Created the token storage schema

--- client/src/components/dashboard/client/ClientDashboard.js
+++ client/src/components/dashboard/client/ClientDashboard.js
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 import Panel from "./Panel";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../dashboard.css';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 // import cookies from "js-cookies";
 // const secure = window.location.protocol === 'https'
@@ -18,7 +18,7 @@ class ClientDashboard extends Component {
 
     render() {
         return (
-            <div className={classNames("d-flex", (this.props.sidebarToggled) ? "toggled":"")} id="dashboard-wrapper">
+            <div className={classNames("d-flex", (this.props.sidebarToggled) ? "toggled" : "")} id="dashboard-wrapper">
                 <Sidebar />
                 <Panel />
             </div>
@@ -26,9 +26,9 @@ class ClientDashboard extends Component {
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     const { clientFlow } = state;
-    return { sidebarToggled:  clientFlow.sidebarToggled}
+    return { sidebarToggled: clientFlow.sidebarToggled }
 }
 
 export default connect(mapStateToProps)(ClientDashboard);
