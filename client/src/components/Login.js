ae604219d6f2e788d1590fabfe192948857260ae
UnitedBagels <unitedbagels@gmail.com>
2021-03-29 16:27:21 -0400

    Added logo and extra touches to login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,6 +9,8 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import rpi_logo from "./login_images/rpi_logo.png"
+import tb_logo from "./login_images/tutorbase_logo.png"
 
 class login extends Component {
   state = {
@@ -62,49 +64,55 @@ class login extends Component {
 
   render() {
     return (
-        <Container fluid="lg" >
-          <div className="loginContainer">
-            <Row>
+        <body class="loginBody">
+          <Container style={{maxWidth:"45em", paddingTop: "5em"}}>
+            <div className="loginContainer">
+              <Row>
+                <Col style={{textAlign:"center", paddingTop: "3em"}}>
+                  <img src={tb_logo} style={{maxWidth: "100%"}} alt="Rensselaer"/>
+                </Col>
+              </Row>
+              <Row>
+                <Col style={{textAlign:"center"}}>
+                  A better system to connect students and tutors at RPI.
+                </Col>
+              </Row>
+              <Row>
+                <Col xs="4"/>
+                <Col xs="4">
+                  <Button onClick={this.redirect} color="danger" size="lg" className="signInButton">Login</Button>
+                </Col>
+                <Col xs="4"/>
+              </Row>
+              <Row>
+                <Col style={{textAlign:"center"}}>
+                  <div className="signUpDiv">
+                    Don't have an account? <Link to="/signup">
+                    &nbsp;Sign up</Link>
+                  </div>
+                </Col>
+              </Row>
+              <Row style={{marginTop: "150px"}}>
+                <Col style={{textAlign:"center"}}>
+                  An <a href="https://rcos.io/"> RCOS</a> project. &nbsp;
+                  <a href="https://github.com/DangItJason/TutorBase">
+                    <img style={{width:"32px", height:"32px"}} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub"/>
+                  </a>
+                </Col>
+                {/*<Media object src={'/'} alt="GitHub" />*/}
+                {/*<img src="login_images/GitHub-Mark-32px.png" alt="GitHub"/>*/}
+              </Row>
+            </div>
+            <Row style={{paddingTop:"10%", paddingBottom:"30%"}}>
               <Col style={{textAlign:"center"}}>
-                <Label className="logoText" >
-                  TutorBase
-                </Label>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{textAlign:"center"}}>
-                A better system to connect students and tutors at RPI.
-              </Col>
-            </Row>
-            <Row>
-              <Col xs="4"/>
-              <Col xs="4">
-                <Button onClick={this.redirect} color="danger" size="lg" className="signInButton">Login</Button>
-              </Col>
-              <Col xs="4"/>
-            </Row>
-            <Row>
-              <Col style={{textAlign:"center"}}>
-                <div className="signUpDiv">
-                  Don't have an account? <Link to="/signup">
-                  &nbsp;Sign up</Link>
-                </div>
-              </Col>
-            </Row>
-            <Row style={{marginTop: "150px"}}>
-              <Col style={{textAlign:"center"}}>
-                An <a href="https://rcos.io/"> RCOS</a> project. &nbsp;
-                <a href="https://github.com/DangItJason/TutorBase">
-                  <img style={{width:"32px", height:"32px"}} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub"/>
+                <a href="https://www.rpi.edu">
+                  <img src={rpi_logo} style={{maxWidth: "100%"}} alt="Rensselaer"/>
                 </a>
               </Col>
+            </Row>
+          </Container>
+        </body>
 
-
-              {/*<Media object src={'/'} alt="GitHub" />*/}
-              {/*<img src="login_images/GitHub-Mark-32px.png" alt="GitHub"/>*/}
-          </Row>
-          </div>
-        </Container>
     );
   }
 }
