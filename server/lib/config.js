427c5f47a8e03f29cba003518aa8fedf5a95ca1d
Chris Faxon <faxonchristopher@gmail.com>
2021-04-09 18:30:07 -0400

    fix: removed email

--- server/lib/config.js
+++ server/lib/config.js
@@ -3,15 +3,15 @@ const nodemailer = require("nodemailer");
 const SMTP_TRANSPORT = {
      service: 'Gmail', // no need to set host or port etc.
      auth: {
-         user: 'entityc22@gmail.com',
-         pass: 'pjqlgbfamedvcwmr'
+         user: 'email@gmail.com',
+         pass: 'appspecificpass'
      }
 };
 
 module.exports = {
   transport: SMTP_TRANSPORT,
   mailOptions: {
-    from: '"TutorBase Team" <entityc22@gmail.com>',
+    from: '"TutorBase Team" <email@gmail.com>',
   },
   debugEnabled: false,
 };
