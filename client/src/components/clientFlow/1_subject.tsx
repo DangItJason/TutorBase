f2e45e037aebde85e422e7d5c8f8a43de635c049
Nick Arnold <narnold@logicbroker.com>
2021-09-28 13:05:48 -0400

    feat: new cards for subject selection

--- client/src/components/clientFlow/1_subject.tsx
+++ client/src/components/clientFlow/1_subject.tsx
@@ -4,13 +4,18 @@ import {actions as clientFlowActions} from "../../store/ClientFlowData/slice";
 import {selectClientFlowData} from "../../store/ClientFlowData/selectors";
 import {api} from "../../services/api";
 import {Subject} from "../../services/api.types";
+import styled from "styled-components";
+import ClientFlowCard from "../clientFlowCard/ClientFlowCard";
 
+/* Step one of the appointment scheduler is for
+   the user to pick their tutor subject */
 export function Step1() {
     let clientFlowData = useSelector(selectClientFlowData);
     let [subjects, setSubjects] = useState<Array<Subject>>([]);
     let dispatch = useDispatch();
 
     useEffect(() => {
+        // Get all avaliable subjects from API
         const getSubjects = async () => {
             return (await api.GetSubjects()).data;
         }
@@ -20,29 +25,41 @@ export function Step1() {
                 dispatch(clientFlowActions.setAvailableSubjects(value));
             }
         )
-    }, [dispatch])
+    }, [])
 
     return (
-        <div className="form-group text-center">
+        <Container>
             <h3 className="hr mt-1">Select a Subject</h3>
-            {subjects.map((subject, i) => (
-                <div className="radio-option" key={i}>
-                    <label>
-                        <input
-                            className="form-input"
-                            type="radio"
-                            name="subject"
-                            value={subject.id}
-                            onChange={(event) => {
-                                dispatch(clientFlowActions.incrementStep());
-                                dispatch(clientFlowActions.setSelectedSubject(subject));
-                            }}
-                            checked={clientFlowData.selectedSubject.id === subject.id}
-                        />
-                        <p className="form-label">{subject.id}</p>
-                    </label>
-                </div>
-            ))}
-        </div>
+            <Cards>
+                {subjects.map((subject, index) => {
+                    return <ClientFlowCard
+                        onClick={() => {
+                            dispatch(clientFlowActions.incrementStep());
+                            dispatch(clientFlowActions.setSelectedSubject(subject));
+                        }}
+                        color={subject.id === "CSCI" ? "blue"
+                            : subject.id === "MATH" ? "green"
+                                : "red"}
+                        title={subject.id}
+                        checked={clientFlowData.selectedSubject.id === subject.id}
+                    ></ClientFlowCard>
+                })}
+            </Cards>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const Cards = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
