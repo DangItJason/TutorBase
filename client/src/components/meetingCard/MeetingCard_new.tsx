8fc32b609625a3c8135442832f150293a9ce3022
Jacob Zamani <jacob.zamani@yahoo.com>
2021-09-28 16:54:57 -0400

    Finished unexpanded card

--- client/src/components/meetingCard/MeetingCard_new.tsx
+++ client/src/components/meetingCard/MeetingCard_new.tsx
@@ -7,7 +7,7 @@ import { Appointment, User } from "../../services/api.types";
 import { api } from "../../services/api";
 
 export function MeetingCard(appt: Appointment) {
-    let [cardtype, setCardtype] = useState<String>(appt.confirmed ? "upcoming-card" : "pending-card");
+    let [cardType, setCardType] = useState<String>(appt.confirmed ? "upcoming-card" : "pending-card");
     let [cardStatus, setcardStatus] = useState<String>(appt.confirmed ? "Upcoming" : "Pending");
     let [cardExpanded, toggleCardExpansion] = useState<boolean>(false);
     let [clientData, setClientData] = useState<User>({
@@ -31,5 +31,27 @@ export function MeetingCard(appt: Appointment) {
             <div className={"card-status"}>{cardStatus}</div>
         </div>
     )
-    return <></>;
+    let upperCardContent = (
+        <>
+          <div className={"card-container-start"}>
+            <div className={"card-name"}>{clientData.first_name + " " + clientData.last_name}</div>
+            <div className={"card-location"}>
+              {appt.location}
+            </div>
+            <div className={"card-time"}>{appt.start_time}</div>
+          </div>
+          {cardTag}
+        </>
+    )
+    let card = (
+        <div 
+            className={"compressed-card " + cardType} 
+            onClick={(e) => {
+                toggleCardExpansion(!cardExpanded)
+            }}
+        >
+            {upperCardContent}
+        </div>
+    );
+    return <>{card}</>;
 }
