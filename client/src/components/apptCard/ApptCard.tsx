56cb1ad3943976146c4665aad77c72edbe8bc563
Jeremy Weiss <jweiss045@gmail.com>
2021-10-01 16:54:16 -0400

    Began  appointment card formatting and styling

--- client/src/components/apptCard/ApptCard.tsx
+++ client/src/components/apptCard/ApptCard.tsx
@@ -1,19 +1,33 @@
+import { Container } from "reactstrap";
+import "./ApptCard.css";
+
 interface IProps {
+    client: string;
     course: string;
     location: string;
     start: string;
     end: string;
 }
 
+const formatDate = (str: string) => {
+    const date = new Date(str);
+    let yr = date.getFullYear();
+    let month = (1 + date.getMonth()).toString().padStart(2, '0');
+    let day = date.getDate().toString().padStart(2, '0');
+  
+    return month + '/' + day + '/' + yr;
+}
 
-export default function ApptCard({course, location, start, end}: IProps) {
+export default function ApptCard({client, course, location, start, end}: IProps) {
     return (
         <div className="card">
-            <p>Course: {course}</p>
-            <p>Start Time: {start}</p>
-            <p>End Time: {end}</p>
-            <p>Location: {location}</p>
+            <Container>
+                <div className="card-client">{client}</div>
+                <div className="card-course">{course}</div>
+                <div className="card-time">{formatDate(start)}</div>
+                <div className="card-location">{location}</div>
+            </Container>
         </div>
-    )
+    );
 }
 
