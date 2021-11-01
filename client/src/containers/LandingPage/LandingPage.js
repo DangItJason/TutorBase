6558817ee99dffec357168b5bdb66ef36928ebed
tamckinney <91158277+tamckinney@users.noreply.github.com>
2021-11-01 12:01:49 -0400

    Update LandingPage.js
    Cut out non functional parts of landing page

--- client/src/containers/LandingPage/LandingPage.js
+++ client/src/containers/LandingPage/LandingPage.js
@@ -46,11 +46,6 @@ class landing extends Component {
                       </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#testimonial">
-                      Testimonial
-                      </a>
-                  </li>
-                  <li className="nav-item">
                     <a className="nav-link" href="#contact">
                       Contact
                       </a>
@@ -70,13 +65,6 @@ class landing extends Component {
                     <h2>On-demand.</h2>
                     <p>Find the help you need with confidence for any offered class on RPI's very first remote, hiring platform for students.</p>
                     <div className="header-button">
-                      <a
-                        rel="nofollow"
-                        href="/signup"
-                        className="btn btn-common"
-                      >
-                        Sign Up Now!
-                        </a>
                       <a
                         href="/login"
                         className="btn btn-border video-popup"
@@ -171,53 +159,7 @@ class landing extends Component {
         </section>
         {/* Services Section End */}
         <div>
-          {/* About Section start */}
-          <div className="about-area section-padding bg-gray">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-6 col-md-12 col-xs-12 info">
-                  <div
-                    className="about-wrapper wow fadeInLeft"
-                    data-wow-delay="0.3s"
-                  >
-                    <div>
-                      <div className="site-heading">
-                        <p className="mb-3">Manage Statistics</p>
-                        <h2 className="section-title">
-                          Detailed Statistics of your Company
-                          </h2>
-                      </div>
-                      <div className="content">
-                        <p>
-                          Praesent imperdiet, tellus et euismod euismod, risus
-                          lorem euismod erat, at finibus neque odio quis metus.
-                          Donec vulputate arcu quam. Morbi quis tincidunt
-                          ligula. Sed rutrum tincidunt pretium. Mauris auctor,
-                          purus a pulvinar fermentum, odio dui vehicula lorem,
-                          nec pharetra justo risus quis mi. Ut ac ex sagittis,
-                          viverra nisl vel, rhoncus odio.
-                          </p>
-                        <a href="#" className="btn btn-common mt-3">
-                          Read More
-                          </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="col-lg-6 col-md-12 col-xs-12 wow fadeInRight"
-                  data-wow-delay="0.3s"
-                >
-                  <img
-                    className="img-fluid"
-                    src="assets/img/about/img-1.png"
-                    alt
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* About Section End */}
+      
           {/* Features Section Start */}
           <section id="features" className="section-padding">
             <div className="container">
