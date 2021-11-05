248888f658097af95af2a730fbe6f7c59b0e70cf
Jeremy Weiss <jweiss045@gmail.com>
2021-11-05 11:21:24 -0400

    Create ConfirmAppointment function to send API request

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -102,6 +102,16 @@ export class ApiService {
 
         return await axios.put(url, body, {withCredentials: true});
     }
+    
+    public async ConfirmAppointment(apptId: String) {
+        let url = this.appointmentsEndpoint;
+        let body = {
+            apptid: apptId,
+            confirmed: true
+        }
+        
+        return await axios.put(url, body, {withCredentials: true});
+    }
 }
 
 export const api = new ApiService();
