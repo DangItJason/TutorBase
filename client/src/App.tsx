845c09fa12ed9d496ca4d51ab45f038a168cf486
Jeremy Weiss <jweiss045@gmail.com>
2022-03-03 11:46:02 -0500

    Landing Page refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import {SignUpPage} from "./containers/SignUpPage/SignupPage";
 import Dashboard from "./containers/DashboardPage/Dashboard";
 import {ToastProvider} from "react-toast-notifications";
 import {BrowserRouter, Route, Switch, useLocation} from "react-router-dom";
 import {LoginPage} from "./containers/LoginPage/LoginPage";
+import {LandingPage} from "./containers/LandingPage/LandingPage";
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/">
-                    <LoginPage/>
+                    <LandingPage/>
                 </Route>
                 <Route exact path="/login">
                     <LoginPage/>
-                </Route>
-                <Route exact path="/signup">
-                    <SignUpPage/>
                 </Route>
                 <Route exact path="/tutor/" >
                     <Dashboard mode="Tutor"/>
